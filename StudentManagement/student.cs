5df0c863b2bc059988efbe5f837cfb5a1e6bf645
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-30 17:06:30 -0400

    Add id field

--- StudentManagement/student.cs
+++ StudentManagement/student.cs
@@ -1,44 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentManagement
+﻿namespace StudentManagement
 {
     public class Student
     {
+        private static int nextId = 1;
 
-        private string Name;
-        private int Sem;
+        private int id;
+        private string name;
+        private int sem;
         private string address;
         private string phoneNumber;
 
-
-    /*    public string Name { get; set; }
-        public int Sem { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; } */
-
         public Student(string name, int sem, string address, string phoneNumber)
         {
-            Name = name;
-            Sem = sem;
+            this.id = nextId++;
+            this.name = name;
+            this.sem = sem;
             this.address = address;
             this.phoneNumber = phoneNumber;
-
         }
 
-        public string Name1 { get => Name; set => Name = value; }
-        public int Sem1 { get => Sem; set => Sem = value; }
+        public int Id { get => id; }
+        public string Name { get => name; set => name = value; }
+        public int Sem { get => sem; set => sem = value; }
         public string Address { get => address; set => address = value; }
         public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
 
         // ToString method to display student details
         public override string ToString()
         {
-            return $"Name: {Name}, Semester: {Sem}, Address: {Address}, Phone: {PhoneNumber}";
+            return $"ID: {Id}, Name: {Name}, Semester: {Sem}, Address: {Address}, Phone: {PhoneNumber}";
         }
-
     }
 }
